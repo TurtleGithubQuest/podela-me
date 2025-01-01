8f234a7c1e92e1e8105d9ac6dbda93ad901329a7
Turtle <github@turtle.quest>
2025-01-01 17:42:18 +0100

    🔨 refactor(modals): Replace profile modal with a reusable base modal and user link partial
    - Remove `profile.html` from `user/modals`
    - Add `base.html` for modal structure
    - Create `user-link.html` for user links with modals
    - Update SCSS for new modal and user link styles
    - Adjust routes and templates to use new partials

--- website/src/page/partials/mod.rs
+++ website/src/page/partials/mod.rs
@@ -1,4 +1,5 @@
 use crate::extend_with_app_state;
+use common::database::user::User;
 use rinja_axum::axum::{
     response::IntoResponse,
     routing::get,
@@ -6,13 +7,20 @@ use rinja_axum::axum::{
 };
 
 extend_with_app_state! {
-    #[template(path = "navbar.html")]
+    #[template(path = "partials/navbar.html")]
     struct NavbarTemplate {};
+
+    #[template(path = "partials/modals/user/profile.html")]
+    struct UserProfileModalTemplate {
+        id: String,
+        profile: Option<User>,
+    };
 }
 
 pub fn router() -> Router<()> {
     Router::new()
         .route("/partials/navbar", get(get::navbar))
+        .route("/partials/modals/user/profile/:id", get(get::modals::profile))
 }
 
 mod get {
@@ -23,4 +31,21 @@ mod get {
         let template = NavbarTemplate::from_app_state(&auth_session);
         rinja_axum::into_response(&template)
     }
+
+    pub(crate) mod modals {
+        use super::*;
+        use axum::extract::Path;
+
+        pub(crate) async fn profile(
+            auth_session: AuthSession,
+            Path(id): Path<String>,
+        ) -> impl IntoResponse {
+            let profile = User::find(id, &auth_session.backend.pool).await;
+            let modal_id = format!("user-link-modal-{}", profile.as_ref().map(|user| user.id.as_str()).unwrap_or("not_found"));
+            let template = UserProfileModalTemplate::from_app_state(&auth_session, modal_id, profile.ok());
+            rinja_axum::into_response(&template)
+        }
+
+    }
+
 }
